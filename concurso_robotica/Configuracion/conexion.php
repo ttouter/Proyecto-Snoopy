e7acce9d28d18a588729735b65c2877b7512b1ee
Luis Roberto Lara Segura <97242444+ttouter@users.noreply.github.com>
2025-12-10 20:19:26 -0600

    v2

--- concurso_robotica/Configuracion/conexion.php
+++ concurso_robotica/Configuracion/conexion.php
@@ -9,6 +9,10 @@ try {
     $dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8";
     $pdo = new PDO($dsn, $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+    // Mensaje de conexión exitosa agregado
+    echo "✅ Conexión exitosa a la base de datos";
+    
 } catch (PDOException $e) {
     // En caso que falle la conexión
     die("❌ Error Crítico de Conexión: " . $e->getMessage());
